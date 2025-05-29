86ea7ac5e53d7817d7068c8245c324a19afde1b4
iamgauravpande <gaupande21@gmail>
2025-05-29 23:13:00 +0530

    Updated code to use OTLP Exporter to Jaeger

--- manual-trace/main.py
+++ manual-trace/main.py
@@ -1,16 +1,15 @@
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-)
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
 # Set up the resource attributes
 resource = Resource(attributes={"service.name": "main", "os-version": "ubuntu-22.04 LTS", "environment": "wsl2"})
 
 provider = TracerProvider(resource=resource)
-processor = BatchSpanProcessor(ConsoleSpanExporter())
+#processor = BatchSpanProcessor(ConsoleSpanExporter())
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="localhost:4317", insecure=True))
 provider.add_span_processor(processor)
 
 # Sets the global default tracer provider
